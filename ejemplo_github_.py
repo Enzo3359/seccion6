4563a89dbdc0f6975f069f8d6af73184178bf511
Enzo3359 <en.moraga@duocuc.cl>
2025-06-26 14:20:39 -0400

    funcion resta

--- ejemplo_github_.py
+++ ejemplo_github_.py
@@ -1,5 +1,11 @@
 def sumar(a,b):
     return a+b 
+
+
+def resta(a,b):
+    return a-b
+
 n1 = int(input("ingresa numero"))
 n2 = int(input("ingresa numero"))
-print(sumar(n1,n2))
+print("la suma es: ", sumar(n1,n2))
+print("la resta es: ",resta(n1,n2))
